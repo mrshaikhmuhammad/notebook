d157863a61073b6edb3e4a6f4ac1d66753f0a53d
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2025-12-01 12:34:40 +0500

    removed id from MetaNotes

--- src/note/MetaNote.java
+++ src/note/MetaNote.java
@@ -1,27 +1,25 @@
 package note;
 
-import java.time.LocalDate;
+import java.time.*;
 import javax.swing.JOptionPane;
 
 public class MetaNote {
-    protected int id;
     protected String title;
     protected String date;
 
     public MetaNote(){
         title = null;
-        date = LocalDate.now().toString();
-        id = -1;
+        date = null;
     }
 
+    public String getDate() {
+        return date;
+    }
+    public void setDate() {
+        this.date = LocalDateTime.now().toString();
+    }
     public String getTitle(){
         return title;
-    }
-    public int getId() {
-        return id;
-    }
-    public void setId(int uniqueId) {
-        this.id = uniqueId;
     }
     public void setTitle(){
         this.title = JOptionPane.showInputDialog("Enter File Name Here: ");
