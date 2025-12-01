0ad08383663d6d8e94139c9705cbcf9cbedcf0cd
mrshaikhmuhammad <mrshaikhmuhammed@gmail.com>
2025-12-01 12:31:33 +0500

    sharing one saveManager for all

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 import gui.components.Frame;
+import store.SaveManager;
 import gui.Notepad;
 
 public class Main {
     private static final String TITLE = "Type Scribe";
     private static final String ICON_ADDRESS = "./src/logo/LogoBlack.png";
     public static void main(String[] args) {
-        Notepad notepad = new Notepad();
+        SaveManager saveManager = new SaveManager();
+        Notepad notepad = new Notepad(saveManager);
         JScrollPane scrollPane = notepad.component();
         Frame screen = new Frame(TITLE, ICON_ADDRESS, scrollPane);
         screen.start();        
